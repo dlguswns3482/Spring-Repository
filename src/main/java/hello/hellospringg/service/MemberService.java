bd6931fc56433147cc92f5163eda071c950791cc
dlguswns3482 <103907181+dlguswns3482@users.noreply.github.com>
2022-12-19 21:17:15 +0900

    회원 서비스 로직 개발

---
+++ src/main/java/hello/hellospringg/service/MemberService.java
@@ -0,0 +1,36 @@
+package hello.hellospringg.service;
+
+import hello.hellospringg.domain.Member;
+import hello.hellospringg.repository.MemberRepository;
+import hello.hellospringg.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원가입
+    public Long join(Member member){
+        // 같은 이름이 있는 회원 X
+        validateDuplicateMember(member); //중복회원검사
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent(member1 -> {
+                            throw new IllegalStateException("이미 존재하는 회원입니다.");
+                        });
+    }
+
+    // 전체회원조회
+    public List<Member> findAllMembers(){
+       return   memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
+
